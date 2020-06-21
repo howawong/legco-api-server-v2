6d3fde3ab60bf0339a791ec550e569aae6014d7a
howa <howa.wong@gmail.com>
2020-06-21 11:23:29 +0800

    Appledaily bug fix

--- jobs/news/classes/graphql.py
+++ jobs/news/classes/graphql.py
@@ -12,6 +12,11 @@ def run_query(query):
     }
     j = {"query": query, "operationName": "MyQuery"}
     resp = requests.post(ENDPOINT, data=json.dumps(j), headers=HEADERS)
-    return resp.json()
+    result = resp.json()
+    errors = result.get("errors", None)
+    if errors is not None:
+        error  = errors[0]
+        raise Exception(error["message"])
+    return result
 
 
